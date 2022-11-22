54555d491728408e21fa74df6b5e7a46e563e26f
AdityaRanjan466 <c0880466@mylambton.ca>
2022-11-21 21:48:31 -0500

    creates SimpleItem

--- src/Box.java
+++ src/Box.java
@@ -1,3 +1,16 @@
-public class Box {
+public class Box extends Item {
 
+    Box [] numOfBoxes = new Box[6];
+    public Box() {
+
+    }
+
+    public void addItem() {
+    }
+
+    public void addItem(SimpleItem pens) {
+    }
+
+    public void addItem(Box box1) {
+    }
 }
--- src/SimpleItem.java
+++ src/SimpleItem.java
@@ -1,3 +1,18 @@
-public class SimpleItem {
+public class SimpleItem extends Item {
+        String scissors;
+        String book;
+        String compass;
+        String scarf;
+        String pencils;
+        String pens;
+        String rubber;
+    public SimpleItem(String book, String pencils, String compass, String scarf, String pens, String rubber ) {
+        this.book = book;
+        this.pencils = pencils;
+        this. compass = compass;
+        this.scarf = scarf;
+        this.pens = pens;
+        this.rubber = rubber;
 
+    }
 }
